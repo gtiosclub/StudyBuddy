b9992d7769940355273ca98dfa9ed78573045e74
Gustavo Garfias <gustavo.egarfias@gmail.com>
2025-02-11 00:11:35 -0500

    Add AI protocols

---
+++ StudyBuddy/Models/AI/IntelligenceManager.swift
@@ -0,0 +1,12 @@
+//
+//  IntelligenceManager.swift
+//  StudyBuddy
+//
+//  Created by Gustavo Garfias on 11/02/2025.
+//
+
+import Foundation
+
+protocol IntelligenceManager {
+    func makeRequest(_ req: IntelligenceRequest) async throws -> IntelligenceResponse
+}
