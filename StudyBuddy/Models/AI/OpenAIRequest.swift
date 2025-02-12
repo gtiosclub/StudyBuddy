5e058e1ecbbf81fc12ee4a2d9405741d7f108f45
Hans Ibarra <Hans.ibarra51@gmail.com>
2025-02-12 10:36:51 -0500

    Add: OpenAIRequest and OpenAIResponse structs

---
+++ StudyBuddy/Models/AI/OpenAIRequest.swift
@@ -0,0 +1,25 @@
+//
+//  OpenAIRequest.swift
+//  StudyBuddy
+//
+//  Created by Hans Ibarra on 2/12/25.
+//
+
+import Foundation
+
+struct OpenAIRequest: Codable {
+    let model: String
+    let messages: [Message]
+    let maxCompletionTokens: Int?
+    let temperature: Double?
+
+    enum CodingKeys: String, CodingKey {
+        case model, messages, temperature
+        case maxCompletionTokens = "max_completion_tokens"
+    }
+}
+
+struct Message: Codable {
+    let role: String
+    let content: String
+}
