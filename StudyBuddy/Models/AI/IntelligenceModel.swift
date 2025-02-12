e8b46db751d14ed882714649948494a8df7c553e
Gustavo Garfias <gustavo.egarfias@gmail.com>
2025-02-11 21:40:23 -0500

    rename models for better readability

--- StudyBuddy/Models/AI/IntelligenceModel.swift
+++ StudyBuddy/Models/AI/IntelligenceModel.swift
@@ -7,8 +7,10 @@
 
 import Foundation
 
+// swiftlint:disable identifier_name
 enum IntelligenceModel {
-    case openai4oMini
-    case llama321b
-    case llama323b
+    case openai_4o_mini
+    case llama_3_2_1b
+    case llama_3_2_3b
 }
+// swiftlint:enable identifier_name
