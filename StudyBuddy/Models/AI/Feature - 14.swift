80f0ce50a718076c9a2d950155e34ca44b9f60b8
Matthew Dong <mattdong1231@gmail.com>
2025-02-11 20:00:59 -0500

    Created a singleton class

---
+++ StudyBuddy/Models/AI/Feature - 14.swift
@@ -0,0 +1,22 @@
+//
+//  Feature - 14.swift
+//  StudyBuddy
+//
+//  Created by Matthew Dong on 2/11/25.
+//
+
+import Foundation
+
+struct LlamaResponse: IntelligenceResponse {
+    var output: String
+}
+
+class LlamaAIManager: IntelligenceManager {
+    static let shared = LlamaAIManager()
+
+    private init() {}
+
+    func makeRequest(_ req: IntelligenceRequest) async throws -> IntelligenceResponse {
+ 
+    }
+}
--- StudyBuddy/Models/AI/IntelligenceManager.swift
+++ StudyBuddy/Models/AI/IntelligenceManager.swift
@@ -10,3 +10,5 @@ import Foundation
 protocol IntelligenceManager {
     func makeRequest(_ req: IntelligenceRequest) async throws -> IntelligenceResponse
 }
+
+
