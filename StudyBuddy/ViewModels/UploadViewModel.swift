5ec8aa297da29f50b2a2a5221c7980752fae45c0
KrishKapoor1329 <krishkapoor1329@gmail.com>
2025-02-13 11:58:55 -0500

    Parse Text

--- StudyBuddy/ViewModels/UploadViewModel.swift
+++ StudyBuddy/ViewModels/UploadViewModel.swift
@@ -9,5 +9,26 @@ import Foundation
 import SwiftUI
 
 class UploadViewModel: ObservableObject {
-
+    @Published var extractedText: [String] = []
+    @Published var isProcessing = false
+    @Published var error: TextRecognizer.ExtractionError?
+    
+    private let textRecognizer = TextRecognizer()
+    
+    func processDocument(at url: URL) {
+        isProcessing = true
+        
+        textRecognizer.extractText(from: url) { [weak self] result in
+            DispatchQueue.main.async {
+                self?.isProcessing = false
+                
+                switch result {
+                case .success(let texts):
+                    self?.extractedText = texts
+                case .failure(let error):
+                    self?.error = error
+                }
+            }
+        }
+    }
 }
