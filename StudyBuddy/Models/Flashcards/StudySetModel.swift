a4a86b44d061acfd2a5249226cf84327bdc73913
dnguyen4637 <160794535+dnguyen4637@users.noreply.github.com>
2025-02-13 19:35:36 -0500

    Function add flashcard and edit falsh card (#41)
    Co-authored-by: Ethan <123336086+quequest@users.noreply.github.com>

--- StudyBuddy/Models/Flashcards/StudySetModel.swift
+++ StudyBuddy/Models/Flashcards/StudySetModel.swift
@@ -7,10 +7,32 @@
 
 import Foundation
 
-class StudySet: ObservableObject {
-    @Published var set: [String: String]
 
-    init(set: [String: String]) {
+class StudySet: ObservableObject {
+    @Published var set: [String: (String, String)]
+    
+    init(set: [String: (String, String)]) {
         self.set = set
+        
     }
+    
+    
+    
+    
+    func add_flashcard(front:String, back:String) {
+        
+        self.set["\(set.count)"] = (front, back)
+    }
+    
+    func edit_flashcard(key:String, front:String, back:String) {
+        
+        self.set[key] = (front,back)
+    }
+    
+   
 }
+
+
+
+    
+   
