4453b7275fe3e6d609107b77b644b874d0dadaba
anishg25 <anish.gajula25@gmail.com>
2025-02-14 20:03:54 -0500

    Added:  a selection option for the user to upload their files private or public  a button underneath to Upload Files.  a go back button if the user misclicked or changed their mind

--- StudyBuddy/Screens/UploadView/UploadView.swift
+++ StudyBuddy/Screens/UploadView/UploadView.swift
@@ -12,7 +12,8 @@ struct UploadView: View {
     @StateObject var uploadViewModel = UploadViewModel()
     // State variable to control document picker presentation
     @State private var isPickerPresented = false
-
+    // State variable to control additional Upload Button
+    @State private var showUploadButton = false
     var body: some View {
         VStack {
             // Button to open document picker
@@ -46,5 +47,66 @@ struct UploadView: View {
         .sheet(isPresented: $isPickerPresented) {
             DocumentPickerView(uploadViewModel: uploadViewModel)
         }
+        
+        VStack (spacing: 20) {
+            Spacer()
+            // if showUploadButton is true, then display two new buttons
+            if showUploadButton {
+                Button { // display upload files button
+                    withAnimation {
+                        showUploadButton = true
+                    }
+                } label: {
+                    Image(systemName:
+                            "square.and.arrow.up")
+                    Text("Upload Files")
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.black)
+                .bold()
+                Button { // displays Go Back button
+                    withAnimation {
+                        showUploadButton = false;
+                    }
+                    
+                }  label: {
+                    Image(systemName:
+                            "chevron.left")
+                    Text("Go Back")
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.red)
+                .bold()
+            } else { // if showUploadButton is false
+                Button { // display public upload option
+                    withAnimation {
+                        showUploadButton = true
+                    }
+                } label: {
+                    Image(systemName:
+                            "square.and.arrow.up")
+                    Text("Upload (Public)")
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.green)
+                .bold()
+
+                Button { // display private upload option
+                    withAnimation {
+                        showUploadButton = true
+                    }
+                } label: {
+                    Image(systemName:
+                            "square.and.arrow.up")
+                    Text("Upload (Private)")
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.blue)
+                .bold()
+            }
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .padding()
+        .animation(.easeInOut, value: showUploadButton)
     }
 }
