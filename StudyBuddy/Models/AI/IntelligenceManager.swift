c527fd43e34e4dec976adf0a229f0dca651ba074
prakashjmitra <mitraprakash06@gmail.com>
2025-02-20 17:40:51 -0500

    mlx first implementation

--- StudyBuddy/Models/AI/IntelligenceManager.swift
+++ StudyBuddy/Models/AI/IntelligenceManager.swift
@@ -7,6 +7,23 @@
 
 import Foundation
 
+
 protocol IntelligenceManager {
     func makeRequest(_ req: IntelligenceRequest) async throws -> IntelligenceResponse
+}
+
+struct AIIntelligenceRequest: IntelligenceRequest {
+    var input: String
+    var model: IntelligenceModel
+}
+
+struct AIIntelligenceResponse: IntelligenceResponse {
+    var output: String
+}
+
+class AIIntelligenceManager: IntelligenceManager {
+    func makeRequest(_ req: IntelligenceRequest) async throws -> IntelligenceResponse {
+        let response = AIIntelligenceResponse(output: "Processed: \(req.input) using model \(req.model)")
+        return response
+    }
 }
