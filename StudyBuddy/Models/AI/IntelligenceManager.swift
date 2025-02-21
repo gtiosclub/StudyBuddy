ae390e4921ad06faec95871b2f00bbb4c2c039e2
Gustavo Garfias <gustavo.egarfias@gmail.com>
2025-02-20 19:35:04 -0500

    small changes to comply with convention

--- StudyBuddy/Models/AI/IntelligenceManager.swift
+++ StudyBuddy/Models/AI/IntelligenceManager.swift
@@ -11,19 +11,3 @@ import Foundation
 protocol IntelligenceManager {
     func makeRequest(_ req: IntelligenceRequest) async throws -> IntelligenceResponse
 }
-
-struct AIIntelligenceRequest: IntelligenceRequest {
-    var input: String
-    var model: IntelligenceModel
-}
-
-struct AIIntelligenceResponse: IntelligenceResponse {
-    var output: String
-}
-
-class AIIntelligenceManager: IntelligenceManager {
-    func makeRequest(_ req: IntelligenceRequest) async throws -> IntelligenceResponse {
-        let response = AIIntelligenceResponse(output: "Processed: \(req.input) using model \(req.model)")
-        return response
-    }
-}
