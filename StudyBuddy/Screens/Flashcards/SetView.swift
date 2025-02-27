deeceffccfd0fd49142c4135032a9907847b2c3a
Calvin DiPrete <calvindiprete@lawn-128-61-34-44.lawn.gatech.edu>
2025-02-27 13:29:05 -0500

    made scrollable view to see flashcards and connects to study view

--- StudyBuddy/Screens/Flashcards/SetView.swift
+++ StudyBuddy/Screens/Flashcards/SetView.swift
@@ -8,13 +8,72 @@
 import SwiftUI
 
 struct SetView: View {
-    var hardcodedSet: [String: String] = ["Hello": "World", "Swift": "UI", "SwiftUI": "Is"]
+    var hardcodedSet: [String: String] = ["Hello": "World", "Swift": "UI", "SwiftUI": "Is", "Card": "definition", "Onemore": "card"]
 
     var body: some View {
-        Text("View All the Words")
+        NavigationStack {
+            ScrollView {
+                VStack(alignment: .leading) {
+                    Text("Flashcards")
+                        .font(.title)
+                        .padding()
+                    
+                    ForEach(hardcodedSet.keys.sorted(), id: \.self)
+                    { key in
+                        HStack { // Loops through the flashcards from hardcodedSet and adds them to the scrollable views
+                            Text(key)
+                                .font(.headline)
+                            Spacer()
+                            Text(hardcodedSet[key] ?? "")
+                                .font(.subheadline)
+                                .foregroundColor(.gray)
+                        }
+                        .padding()
+                        .background(Color(.systemGray5))
+                        .cornerRadius(8)
+                    }
+
+                }
+                .safeAreaInset(edge: .bottom) { // Idea was to pin the button to the bottom but it isnt doing that
+                    NavigationLink(destination: StudyView()) {
+                    Text("Study")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background(Color.blue)
+                        .cornerRadius(10)
+                }
+                .padding(.top, 20)
+                }
+                .padding()
+            }
+        }
     }
 }
+// Next to do: Add button that takes you to study view from this one.
 
 #Preview {
-    SetView()
+SetView()
 }
+
+
+
+
+
+
+
+
+
+
+
+
+//
+    //    var body: some View {
+    //        Text("View All the Words")
+    //    }
+    //}
+    //
+    //#Preview {
+    //    SetView()
+    //}
