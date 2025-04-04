d76b3ad3790a7493d2e70867c0dbbba0dd23b50d
seanyse <seanyan0915@gmail.com>
2025-04-03 20:24:30 -0400

    updated flashcardview

--- StudyBuddy/Screens/Flashcards/StudyView.swift
+++ StudyBuddy/Screens/Flashcards/StudyView.swift
@@ -13,65 +13,151 @@ struct StudyView: View {
     @State private var flashCardIndex = 0
     @State private var showBack = false
     @State private var currentText: String = ""
-    
+    @State private var progress: Double = 0.4 // controls status bar
+    @State private var master: Int = 0
+    @State private var unmaster: Int = 0
+    @State private var dragOffset = CGSize.zero // for rectangle dragging
+
     init(studySet: StudySet) {
         self.studySet = studySet
     }
-    
+
     var body: some View {
         VStack {
-            Spacer()
+            topNavView()
+            topStatusView()
             Spacer()
             cardView()
             Spacer()
             navBar()
             Spacer()
         }
-
     }
+
+    private func topNavView() -> some View {
+        HStack {
+            Button(action: {
+                print("return") // go back not implemented yet
+            }) {
+                HStack {
+                    Image(systemName: "xmark")
+                }
+            }
+            
+            Spacer()
+            Text("14/48") // actual progress not implemented yet
+                .font(.headline)
+            Spacer()
+        }
+        .padding(.horizontal, 25)
+        .padding(.vertical, 10)
+    }
+
+    private func topStatusView() -> some View {
+        HStack {
+            VStack {
+                ZStack(alignment: .leading) {
+                    RoundedRectangle(cornerRadius: 0)
+                        .fill(Color.gray)
+                        .fill(Color.gray.opacity(0.3))
+                        .frame(height: 5)
+                    RoundedRectangle(cornerRadius: 5)
+                        .fill(Color.blue)
+                        .frame(width: (progress * 400), height: 6)
+                }
+            }
+        }
+    }
+
     private func cardView() -> some View {
         ZStack {
             RoundedRectangle(cornerRadius: 20)
-                .fill(Color.blue)
-                .frame(width: 300, height: 200)
+                .fill(Color.green)
+                .frame(width: 175, height: 600)
                 .shadow(radius: 5)
-            Text((showBack ? studySet.set["\(flashCardIndex)"]?.1 ?? "back" : studySet.set["\(flashCardIndex)"]?.0) ?? "front")
-                    .font(.title)
-        }
-        .onTapGesture {
-            showBack.toggle()
-        }
+                .overlay(
+                    Text("Mastered")
+                )
+                .offset(x: dragOffset.width - 350, y: 0) // makes the rectangle outside the user view
+            
+            RoundedRectangle(cornerRadius: 20)
+                .fill(Color.red)
+                .frame(width: 175, height: 600)
+                .shadow(radius: 5)
+                .overlay(
+                    Text("Not Mastered")
+                )
+                .offset(x: dragOffset.width + 350, y: 0) // makes the rectangle outside the user view
+            
+            RoundedRectangle(cornerRadius: 20)
+                .fill(Color.blue)
+                .frame(width: 375, height: 600)
+                .shadow(radius: 5)
+                .overlay(
+                    Text((showBack ? studySet.set["\(flashCardIndex)"]?.1 ?? "back" : studySet.set["\(flashCardIndex)"]?.0) ?? "front")
+                        .font(.title)
+                        .foregroundColor(.black)
+                )
+                .offset(x: dragOffset.width)
+                .gesture(
+                    DragGesture()
+                        .onChanged { value in
+                            dragOffset = value.translation
+                        }
+                        .onEnded { value in
+                            withAnimation(.spring()) {
+                                // makes sure drag is not too out of bounds
+                                dragOffset.width = min(max(dragOffset.width, -260), 260)
 
+                                if dragOffset.width > 250 {
+                                    // implementation for mastering term
+                                    print("right")
+                                    master += 1 // call model
+                                    flashCardIndex += 1
+                                } else if dragOffset.width < -250 {
+                                    // implementation for struggling term
+                                    print("left")
+                                    unmaster += 1 // call model
+                                    flashCardIndex += 1
+                                }
+                                
+
+                                // Smoothly reset the drag offset with animation
+                                dragOffset = .zero
+                            }
+                        }
+                )
+                .onTapGesture {
+                    showBack.toggle()
+                }
+        }
     }
+
     private func navBar() -> some View {
         HStack {
-
-            Button(action: {
-                if flashCardIndex > 0 {
-                    flashCardIndex -= 1
-                    showBack = false
-                }
-            }) {
-                HStack {
-                    Image(systemName: "arrow.left")
-                    Text("Left")
-                }
+            HStack {
+                Image(systemName: "checkmark.seal")
+                Text("\(master)")
             }
-            Button(action: {
-                if flashCardIndex < studySet.set.count {
 
-                    flashCardIndex += 1
-                    showBack = false
-                }
+            HStack {
+                Image(systemName: "xmark.seal")
+                Text("\(unmaster)")
+            }
+
+            Spacer()
+
+            Button(action: {
+                showBack.toggle()
             }) {
                 HStack {
-                    Text("Right")
-                    Image(systemName: "arrow.right")
+                    Image(systemName: "return")
+                    Text("Undo")
                 }
             }
         }
+        .padding(20)
     }
-
 }
 
 #Preview {
