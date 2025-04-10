49b8550e8ce18844f7c60ceec59b3c1ea81ed486
Gustavo Garfias <gustavo.egarfias@gmail.com>
2025-04-10 18:33:14 -0400

    Add make flashcards functionality

--- StudyBuddy/Models/AI/OpenAIManager.swift
+++ StudyBuddy/Models/AI/OpenAIManager.swift
@@ -27,6 +27,9 @@ final class OpenAIManager: IntelligenceManager {
         }
         
         let jsonData = try JSONEncoder().encode(requestData)
+//        if let jsonString = String(data: jsonData, encoding: .utf8) {
+//            print(jsonString)
+//        }
 
         var request = URLRequest(url: baseURL)
         request.httpMethod = "POST"
@@ -50,8 +53,54 @@ final class OpenAIManager: IntelligenceManager {
 
         return OpenAIIntelligenceResponse(output: output)
     }
+
+    // swiftlint:disable:next function_body_length
+    func makeFlashcards(content: [String]) async throws -> [(String, String)] {
+        var messages: [OpenAIRequest.Message] = [
+            .init(role: "system", content: """
+            Your task is condensing the information from all the source documents and identifying key concepts that a student would need to learn for an exam. 
+            The end goal is creating flashcards for each of the key concepts.
+            Each flashcard should have a question and an answer.
+            """)
+        ]
+
+        for document in content {
+            messages.append(.init(role: "user", content: document))
+        }
+
+        let flashcardRequest = OpenAIRequest(
+            input: "",
+            model: .openai_4o_mini,
+            messages: messages,
+            maxCompletionTokens: 1000,
+            temperature: 0.7,
+            responseFormat: OpenAIRequest.flashcardResponseFormatStruct
+        )
+
+        let response = try await self.makeRequest(flashcardRequest)
+
+        let flashcardResponse =
+        try JSONDecoder()
+            .decode(
+                OpenAIFlashcardResponse.self,
+                from: response.output.data(using: .utf8)!
+            )
+
+        let flashcards = flashcardResponse.flashcards.map { ($0.question, $0.answer) }
+
+        return flashcards
+    }
 }
 
 struct OpenAIIntelligenceResponse: IntelligenceResponse {
     var output: String
+}
+
+struct OpenAIFlashcardResponse: Decodable {
+    let flashcards: [OpenAIFlashcard]
+}
+
+struct OpenAIFlashcard: Decodable {
+    let question: String
+    let answer: String
 }
--- StudyBuddy/Models/AI/OpenAIRequest.swift
+++ StudyBuddy/Models/AI/OpenAIRequest.swift
@@ -13,14 +13,99 @@ struct OpenAIRequest: Encodable, IntelligenceRequest {
     let messages: [Message]
     let maxCompletionTokens: Int?
     let temperature: Double?
+    var responseFormat: ResponseFormat?
 
     enum CodingKeys: String, CodingKey {
         case model, messages, temperature
         case maxCompletionTokens = "max_completion_tokens"
+        case responseFormat = "response_format"
     }
 
     struct Message: Codable {
         let role: String
         let content: String
+    }
+
+    static let flashcardResponseFormatStruct: ResponseFormat = .init(
+        type: "json_schema",
+        jsonSchema: .init(
+            name: "flashcards",
+            strict: true,
+            schema: .init(
+                type: "object",
+                properties: .init(
+                    flashcards: .init(
+                        type: "array",
+                        items: .init(
+                            type: "object",
+                            properties: .init(
+                                question: .init(type: "string"),
+                                answer: .init(type: "string")),
+                            required: [
+                                "question",
+                                "answer"
+                            ],
+                            additionalProperties: false
+                        )
+                    )
+                ),
+                required: [
+                    "flashcards"
+                ],
+                additionalProperties: false
+            )
+        )
+    )
+
+    struct ResponseFormat: Encodable {
+        let type: String
+        let jsonSchema: JsonSchema
+
+        enum CodingKeys: String, CodingKey {
+            case type
+            case jsonSchema = "json_schema"
+        }
+
+        struct JsonSchema: Encodable {
+            let name: String
+            let strict: Bool
+            let schema: Schema
+
+            struct Schema: Encodable {
+                let type: String
+                let properties: Properties
+                let required: [String]
+                let additionalProperties: Bool
+
+                struct Properties: Encodable {
+                    let flashcards: Flashcards
+
+                    struct Flashcards: Encodable {
+                        let type: String
+                        let items: Item
+
+                        struct Item: Encodable {
+                            let type: String
+                            let properties: Properties
+                            let required: [String]
+                            let additionalProperties: Bool
+
+                            struct Properties: Encodable {
+                                let question: Question
+                                let answer: Answer
+
+                                struct Question: Encodable {
+                                    let type: String
+                                }
+
+                                struct Answer: Encodable {
+                                    let type: String
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
