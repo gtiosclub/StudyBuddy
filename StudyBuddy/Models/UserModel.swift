944ee303c8a4072588ad25e3a4cad17b65dfa584
Ethan Kim <jikim1803@gmail.com>
2025-04-13 09:40:01 -0400

    changes

--- StudyBuddy/Models/UserModel.swift
+++ StudyBuddy/Models/UserModel.swift
@@ -24,9 +24,9 @@ struct UserModel: Codable, Identifiable {
 func createTestUser(_ email: String, _ userName: String, _ date1: Date, _ date2: Date, _ date3: Date, _ flashcardText1: String, _ flashcardText2: String, _ flashcardText3: String) -> UserModel {
     var user = UserModel(email: email, userName: userName, firstName: "Test", lastName: "User")
 
-    let flashcard1 = FlashcardModel(front: text1[0], back: text1[1], createdBy: user.userName, mastered: false)
-    let flashcard2 = FlashcardModel(front: text2[0], back: text2[1], createdBy: user.userName, mastered: false)
-    let flashcard3 = FlashcardModel(front: text3[0], back: text3[1], createdBy: user.userName, mastered: false)
+    let flashcard1 = FlashcardModel(front: "front", back: "back", createdBy: user.userName, mastered: false)
+    let flashcard2 = FlashcardModel(front: "front", back: "back", createdBy: user.userName, mastered: false)
+    let flashcard3 = FlashcardModel(front: "front", back: "back", createdBy: user.userName, mastered: false)
 
 
     user.studySets = [
@@ -52,10 +52,7 @@ let date3 = createDate(date: "2024-02-28")
 let flashcardText1 = "test123"
 let flashcardText2 = "test455"
 let flashcardText3 = "testagain"
-let fback1 = "back1"
-let fback2 = "back2"
-let fback3 = "back3"
-var user1 = createTestUser("bob123@gmail.com", "bob123", date1, date2, date3, [flashcardText1, fback1], [flashcardText2, fback2], [flashcardText3, fback3])
+var user1 = createTestUser("bob123@gmail.com", "bob123", date1, date2, date3, flashcardText1, flashcardText2, flashcardText3)
 
 let date4 = createDate(date: "2021-07-13")
 let date5 = createDate(date: "2019-10-13")
@@ -63,10 +60,7 @@ let date6 = createDate(date: "2010-11-28")
 let flashcardText4 = "abcd123"
 let flashcardText5 = "efjh456"
 let flashcardText6 = "ijkl789"
-let fback4 = "back1"
-let fback5 = "back2"
-let fback6 = "back3"
-var user2 = createTestUser("jim456@gmail.com", "jim456", date4, date5, date6, [flashcardText4, fback4], [flashcardText5, fback5], [flashcardText6, fback6])
+var user2 = createTestUser("jim456@gmail.com", "jim456", date4, date5, date6, flashcardText1, flashcardText2, flashcardText3)
 
 let date7 = createDate(date: "2005-12-25")
 let date8 = createDate(date: "2014-09-23")
@@ -74,7 +68,4 @@ let date9 = createDate(date: "2023-06-05")
 let flashcardText7 = "falfa324"
 let flashcardText8 = "1+1=2"
 let flashcardText9 = "2^2=4"
-let fback7 = "back7"
-let fback8 = "back8"
-let fback9 = "back9"
-var user3 = createTestUser("billy789@gmail.com", "billy789", date7, date8, date9, [flashcardText7, fback7], [flashcardText8, fback8], [flashcardText9, fback9])
+var user3 = createTestUser("billy789@gmail.com", "billy789", date7, date8, date9, flashcardText7, flashcardText8, flashcardText9)
